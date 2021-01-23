ca4dd2a88ec2b255e9124ea297582e6a9704f21a
Enhisir <enhisir@gmail.com>
2021-01-24 01:11:48 +0500

    added base functional

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
 import os
 import pygame
 from globals import load_image
-from units import Player
+from units import Player, BaseEnemy
 
 
 pygame.init()
+
+
+class HealthBar:
+    def __init__(self, player: Player, screen: pygame.surface.Surface):
+        self.image = load_image("hp.png", screen)
+        self.screen = screen
+        self.player = player
+        self.rect = pygame.rect.Rect(845, 25, self.image.get_width(),
+                                     self.image.get_height())
+        self.space = 5
+
+    def draw(self) -> None:
+        hp = self.player.get_health()
+        if hp:
+            for i in range(hp):
+                self.screen.blit(
+                    self.image,
+                    (self.rect.x + self.rect.w * i + self.space * i, self.rect.y)
+                )
+
+
+class Score:
+    font = pygame.font.Font(os.path.join("res", "sw_font.ttf"), 45)
+
+    def __init__(self, screen: pygame.surface.Surface):
+        self.screen = screen
+        self.x = self.y = 25
+        self.count = 0
+
+    def add(self, number: int) -> None:
+        self.count += number
+
+    def draw(self) -> None:
+        text = Score.font.render(f"SCORE: {self.count}", True, "#FFD700")
+        self.screen.blit(text, (self.x, self.y))
 
 
 class Button:
     def __init__(self, x: int, y: int, screen: pygame.surface.Surface, image_filename: str):
         # инициализация интерфейса для общения с кнопкой
         self.image = load_image(image_filename, screen)
+        self.screen = screen
         self.x, self.y = x, y
         self.rect = pygame.rect.Rect(x, y, self.image.get_width(), self.image.get_height())
 
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
 
 class Game:
-    FPS = 60
+    FPS = 240
     SIZE = WIDTH, HEIGHT = 1000, 1000
     SW_FONT_MAIN = pygame.font.Font(os.path.join("res", "sw_font.ttf"), 80) # кастомный шрифт
+    SCOREEVENT = pygame.USEREVENT + 1
 
     def __init__(self):
         self.screen = pygame.display.set_mode(Game.SIZE)
@@ -51,8 +91,8 @@ class Game:
             # инициализация главного меню
             self.screen.blit(self.bg_image, (0, 0))
             self.screen.blit(text, (42, 100))
-            self.screen.blit(start_button.image, (start_button.x, start_button.y))
-            self.screen.blit(exit_button.image, (exit_button.x, exit_button.y))
+            start_button.draw()
+            exit_button.draw()
 
             self.clock.tick(Game.FPS)
             pygame.display.flip()
@@ -61,20 +101,34 @@ class Game:
 
     def main_activity(self) -> None:
         running = True
+        pygame.time.set_timer(Game.SCOREEVENT, 500)
 
         all_sprites = pygame.sprite.Group()
         player_group = pygame.sprite.Group()
 
         player = Player(425, 800, self.screen, all_sprites, player_group)
+
+        score = Score(self.screen)
+        hp_bar = HealthBar(player, self.screen)
+
         while running:
             time = self.clock.tick(Game.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                    player.hp -= 1
+                    print(player.get_health())
+                if event.type == Game.SCOREEVENT:
+                    score.add(1)
+
             player_group.update(time)
 
             self.screen.blit(self.bg_image, (0, 0))
             player_group.draw(self.screen)
+
+            score.draw()
+            hp_bar.draw()
 
             pygame.display.flip()
 
--- units.py
+++ units.py
@@ -16,12 +16,22 @@ class Character(pygame.sprite.Sprite):
 
 class Player(Character):
     velocity = 500
+    power = 1
 
     def __init__(self, x: int, y: int, screen: pygame.surface.Surface, *group):
         super().__init__(x, y, screen, *group)
+        self.hp = 3
 
     def get_image(self, screen: pygame.surface.Surface) -> pygame.surface.Surface:
         return load_image("player.png", screen)
+
+    def get_health(self) -> int:
+        return self.hp
+
+    def hurt(self, other):
+        self.hp -= other.power
+        if self.hp < 0:
+            self.hp = 0
 
     def update(self, time: int) -> None:
         keys = pygame.key.get_pressed()
@@ -30,9 +40,9 @@ class Player(Character):
         elif keys[pygame.K_s]:
             self.rect = self.rect.move(0, Player.velocity * time / 1000)
 
-        if self.rect.y < 10:
-            self.rect.y = 10
-        elif self.rect.y + self.rect.h >= self.screen.get_height() - 10:
+        if self.rect.y < 50:
+            self.rect.y = 50
+        elif self.rect.y + self.rect.h > self.screen.get_height() - 10:
             self.rect.y = self.screen.get_height() - 10 - self.rect.h
 
         if keys[pygame.K_a]:
@@ -44,3 +54,7 @@ class Player(Character):
             self.rect.x = 10
         elif self.rect.x + self.rect.w >= self.screen.get_width() - 10:
             self.rect.x = self.screen.get_width() - 10 - self.rect.w
+
+
+class BaseEnemy(Character):
+    power = 1
